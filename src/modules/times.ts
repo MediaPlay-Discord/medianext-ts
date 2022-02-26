54daf9450bb19fe37fd5f578ca542e0d8434e03b
darksub2004 <nikitapakhomova1509@gmail.com>
2022-02-27 01:59:39 +0500

    Change

--- src/modules/times.ts
+++ src/modules/times.ts
@@ -1,14 +1,18 @@
+/* eslint-disable one-var */
+/* eslint-disable no-var */
 import * as util from 'util';
 export const wait = util.promisify(setTimeout);
 
 export function convertMS(ms: number) {
-	const days = Math.floor(ms / 86400000);
+	var days, hours, minutes, seconds;
+
+	days = Math.floor(ms / 86400000);
 	ms %= 86400000;
-	const hours = Math.floor(ms / 3600000);
+	hours = Math.floor(ms / 3600000);
 	ms %= 3600000;
-	const minutes = Math.floor(ms / 60000);
+	minutes = Math.floor(ms / 60000);
 	ms %= 60000;
-	const seconds = Math.floor(ms / 1000);
+	seconds = Math.floor(ms / 1000);
 	ms %= 1000;
 
 	return `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
